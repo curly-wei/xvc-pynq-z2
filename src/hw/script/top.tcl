4a424f531889dba8790cc05f546906772a1cd070
curlywei <curlywei@gmail.com>
2021-05-07 15:33:37 +0800

    add INFO for top.tcl of hw

--- src/hw/script/top.tcl
+++ src/hw/script/top.tcl
@@ -72,7 +72,9 @@ proc num_threads_run {} {
 }
 
 set_param general.maxThreads [num_threads_run]
+puts "=================================================================="
 puts "INFO: Max threads = [get_param general.maxThreads] "
+puts "=================================================================="
 
 ## STEP#1: setup design sources and constraints
 #
@@ -81,7 +83,9 @@ foreach dirs ${kVerilogSrcDirs} {
   set verilog_files [ glob -nocomplain "${dirs}/*.v" ]
   if { ${verilog_files} != "" } {
     read_verilog ${verilog_files}
+    puts "=================================================================="
     puts "INFO: read-in verilog files: ${verilog_files}"
+    puts "=================================================================="
   } 
 }
 
@@ -90,7 +94,9 @@ foreach dirs ${kXDCSrcDirs} {
   set xdc_files [ glob -nocomplain "${dirs}/*.xdc" ]  
   if { ${xdc_files} != "" } {
     read_xdc ${xdc_files}
+    puts "=================================================================="
     puts "INFO: read-in xdc files: ${xdc_files}"
+    puts "=================================================================="
   }
 }
 
@@ -101,7 +107,9 @@ foreach dirs ${kIPSrcDirs} {
   if { ${ip_files} != "" } {
     read_ip ${ip_files}
     set ${has_ip_files} 1
+    puts "=================================================================="
     puts "INFO: read-in ip files: ${ip_files}"
+    puts "=================================================================="
   }
 }
 
@@ -115,7 +123,9 @@ if { ${has_ip_files} == 1 } {
 #
 # Load ps preset
 if { [ file exists ${kPSPresetFile} ] == 1 } {
+  puts "=================================================================="
   puts "INFO: read-in files for ps preset: ${kPSPresetFile}"
+  puts "=================================================================="
   source ${kPSPresetFile}
 } else {
   error "ERROR: read-in files for ps preset: < ${kPSPresetFile} > fail. \
@@ -125,7 +135,9 @@ if { [ file exists ${kPSPresetFile} ] == 1 } {
 # create the BD-Design
 if { [ file exists ${kTopBDScriptFile} ] == 1 } {
   # read-in bd-created shell and execute it
+  puts "=================================================================="
   puts "INFO: read-in files for top bd: ${kTopBDScriptFile}"
+  puts "=================================================================="
   source ${kTopBDScriptFile}
   init_xcv_system_bd ${kBDName}
   create_root_design ""
@@ -153,11 +165,16 @@ read_verilog ${top_bd_wrapper_path}
 # See
 # https://www.xilinx.com/support/answers/51688.html
 #
+puts "=================================================================="
+puts "INFO: Run Synthesis"
+puts "=================================================================="
 reorder_files -auto
 synth_design -top ${top_bd_wrapper_name} -part ${kFPGAPart} -flatten rebuilt 
 
+puts "=================================================================="
+puts "INFO: Genetrate dcp/Report for Synthesis"
+puts "=================================================================="
 write_checkpoint -force "${kOutputDir}/post_synth.dcp"
-
 report_timing_summary -file "${kOutputDir}/post_synth_timing_summary.rpt"
 report_power -file "${kOutputDir}/post_synth_power.rpt"
 report_compile_order -file "${kOutputDir}/post_synth_compile_order.rpt"
@@ -166,22 +183,32 @@ report_compile_order -file "${kOutputDir}/post_synth_compile_order.rpt"
 # run placement and logic optimzation, 
 # report utilization and timing estimates, write checkpoint design
 #
+puts "=================================================================="
+puts "INFO: Run Implementions"
+puts "=================================================================="
 opt_design
 place_design
 phys_opt_design
 
+puts "=================================================================="
+puts "INFO: Genetrate dcp/Report for Implementions"
+puts "=================================================================="
 #write_checkpoint -force "${kOutputDir}/post_place"
-
 report_timing_summary -file "${kOutputDir}/post_place_timing_summary.rpt"
 
 # STEP#5: 
 # run router, report actual utilization and timing, 
 # write checkpoint design, run drc, write verilog and xdc out
 #
+puts "=================================================================="
+puts "INFO: Run Route"
+puts "=================================================================="
 route_design
-
 write_checkpoint -force "${kOutputDir}/post_route"
 
+puts "=================================================================="
+puts "INFO: Genetrate dcp/Report for Route"
+puts "=================================================================="
 report_timing_summary -file "${kOutputDir}/post_route_timing_summary.rpt"
 report_timing -sort_by group -max_paths 100 -path_type summary -file \
   "${kOutputDir}/post_route_timing.rpt"
@@ -190,11 +217,17 @@ report_utilization -file "${kOutputDir}/post_route_util.rpt"
 report_power -file "${kOutputDir}/post_route_power.rpt"
 report_drc -file "${kOutputDir}/post_imp_drc.rpt"
 
+puts "=================================================================="
+puts "INFO: Genetrate Summrized xdc/hdl file"
+puts "=================================================================="
 write_verilog -force "${kOutputDir}/${kBDName}_top_impl_netlist.v"
 write_xdc -no_fixed_only -force "${kOutputDir}/${kBDName}_top_impl.xdc"
 
 # STEP#6: generate a bitstream
 # 
+puts "=================================================================="
+puts "INFO: Genetrate Bitstream and debug info"
+puts "=================================================================="
 write_bitstream -force "${kOutputDir}/${kBDName}_top.bit"
 write_debug_probes -force "${kOutputDir}/${kBDName}_top.itx"
 
@@ -206,6 +239,10 @@ write_debug_probes -force "${kOutputDir}/${kBDName}_top.itx"
 # furthermore, can't use read_bd ${bd_file_and_path}
 # reason Refer to
 # https://www.xilinx.com/support/answers/60945.html
+#
+puts "=================================================================="
+puts "INFO: Genetrate xsa File"
+puts "=================================================================="
 open_checkpoint "${kOutputDir}/post_route.dcp"
 set_property platform.design_intent.embedded true [current_project]
 set_property platform.design_intent.server_managed false [current_project]
