280a3df88320bc1cc2c74c428775c56496dee52b
curlywei <curlywei@gmail.com>
2021-05-11 18:22:17 +0800

    mof hdl file

--- src/hw/hdl/jtag_proc.v
+++ src/hw/hdl/jtag_proc.v
@@ -1,202 +1,189 @@
 `timescale 1 ns / 1 ps
 
-//-----------------------------------------------------------------------------
-//
-// Disclaimer:
-//    Avnet, Inc. makes no warranty for the use of this code or design.
-//    This code is provided  "As Is". Avnet, Inc assumes no responsibility for
-//    any errors, which may appear in this code, nor does it make a commitment
-//    to update the information contained herein. Avnet, Inc specifically
-//    disclaims any implied warranties of fitness for a particular purpose.
-//                     Copyright(c) 2014-2015 Avnet, Inc.
-//                             All rights reserved.
-//-----------------------------------------------------------------------------
-
 module jtag_proc # (
-   parameter integer C_TCK_CLOCK_RATIO = 8
-)
-(
-   input  wire           CLK        ,
-   input  wire           RESET      ,
-   input  wire           ENABLE     ,
-   output reg            DONE       ,
-   input  wire [31 : 0 ] LENGTH     ,
-   input  wire [31 : 0 ] TMS_VECTOR ,
-   input  wire [31 : 0 ] TDI_VECTOR ,
-   output wire [31 : 0 ] TDO_VECTOR ,
-   output wire           TCK        ,
-   output wire           TMS        ,
-   output wire           TDI        ,
-   input  wire           TDO       
+  parameter integer C_TCK_CLOCK_RATIO = 8
+) (
+  input  wire        clk_i,
+  input  wire        reset_i,
+  input  wire        en_i,
+  output reg         done_o,
+  input  wire [31:0] length_i,
+  input  wire [31:0] tms_vec_i,
+  input  wire [31:0] tdi_vec_i,
+  output wire [31:0] tod_vec_o,
+  output wire        tck_o,
+  output wire        tms_o,
+  output wire        tdi_o,
+  input  wire        tdo_i       
 );
 
-   localparam  IDLE = 3'b001 ,
-               TCKL = 3'b010 ,
-               TCKH = 3'b100 ;           
+localparam Ideal_lk = 3'b001 ,
+localparam TCKL_lk  = 3'b010 ,
+localparam TCKH_lk  = 3'b100 ;           
 
-   reg [ 2:0]   state, next_state;
-   reg          enable_d;
-   wire         enable_red;
-   reg          tck_en;
-   reg          tck_i;
-   reg          done_i;
-   reg [ 7:0]   tck_count;
-   reg [31:0]   bit_count;
-   reg [31:0]   tms_output;
-   reg [31:0]   tdi_output;
-   reg [31:0]   tdo_capture;
-   
-   reg [0:0]    tdo_buffer [31:0];
-   reg [4:0]    index;
-   
-   wire         tck_pulse;
-   wire [31:0] tdo_capture2;
-   
-   always @(posedge CLK) begin
-      if (RESET == 1'b1) begin
-         state    <= IDLE;
-         enable_d <= 1'b0;
-      end else begin
-         state    <= next_state;
-         enable_d <= ENABLE;
+reg [2:0]    state, next_state;
+reg          en_r;
+wire         en_edge;
+reg          tck_en;
+reg          tck_r;
+reg          done_r;
+reg [7:0]    tck_cnt;
+reg [31:0]   bit_cnt;
+reg [31:0]   tms_r;
+reg [31:0]   tdi_r;
+reg [31:0]   tdo_capture;
+
+reg [0:0]    tdo_buffer [31:0];
+reg [4:0]    index;
+
+wire         tck_pulse;
+wire [31:0]  tdo_capture2;
+
+always @(posedge clk_i) begin: state_transfer
+  if (reset_i == 1'b1) begin
+    state    <= Ideal_lk;
+    en_r <= 1'b0;
+  end else begin
+    state    <= next_state;
+    en_r <= en_i;
+  end
+end
+
+assign en_edge = en_i && !en_r;
+
+always @(state or en_edge or bit_cnt or tck_pulse) begin 
+  next_state = state;
+  tck_en     = 1'b0;
+  done_r     = 1'b0;
+  
+  case (state)
+    Ideal_lk: begin
+      if (en_edge == 1'b1) begin
+        next_state = TCKL_lk;
+        tck_en = 1'b1;
       end
-   end
-   
-   assign enable_red = ENABLE && !enable_d;
-   
-   always @(state or enable_red or bit_count or tck_pulse) begin 
-      next_state = state;
-      tck_en     = 1'b0;
-      done_i     = 1'b0;
-      case (state)
-         IDLE : begin
-            if (enable_red == 1'b1) begin
-               next_state = TCKL;
-               tck_en      = 1'b1;
-            end
-         end
-         
-         TCKL : begin
-            tck_en     = 1'b1;
-            if (tck_pulse == 1'b1) begin
-               next_state = TCKH;
-            end else begin
-               next_state = TCKL;
-            end 
-         end
-         
-         TCKH : begin
-            tck_en     = 1'b1;
-            if (tck_pulse == 1'b1) begin
-               if (bit_count == 0) begin
-                  next_state = IDLE;
-                  done_i     = 1'b1;
-               end else begin
-                  next_state = TCKL;
-               end
-            end else begin
-               next_state = TCKH;
-            end 
-         end
-         
-         default : begin
-            next_state = IDLE;
-         end
-      endcase
-   end
-   
-   always @(posedge CLK) begin
-      if (RESET == 1'b1) begin
-         tck_count <= 0;
-         bit_count <= 0;
-         index     <= 0;
+    end
+    
+    TCKL_lk: begin
+      tck_en = 1'b1;
+      if (tck_pulse == 1'b1) begin
+        next_state = TCKH_lk;
       end else begin
-         if (enable_red == 1'b1) begin
-            tck_count <= 0;
-            bit_count <= LENGTH-1;
-            index     <= 0;
-         end else if (tck_en == 1'b1) begin
-            if (tck_count == (C_TCK_CLOCK_RATIO/2)-1) begin
-               tck_count <= 0;
-            end else begin
-               tck_count <= tck_count + 1;
-            end
-            //
-            if (state == TCKH && tck_pulse) begin
-               bit_count <= bit_count - 1;
-               index     <= index + 1;
-            end
-         end
+        next_state = TCKL_lk;
       end
-   end
-   
-   assign tck_pulse = (tck_count == (C_TCK_CLOCK_RATIO/2)-1) ? 1'b1 : 1'b0;
-   
-   always @(posedge CLK) begin
-      if (RESET == 1'b1) begin
-         tck_i       <= 1'b0;
-         tms_output  <= 0;
-         tdi_output  <= 0;
-         tdo_capture <= 0;
-         DONE        <= 1'b0;
+    end
+    
+    TCKH_lk: begin
+      tck_en = 1'b1;
+      if (tck_pulse == 1'b1) begin
+        if (bit_cnt == 0) begin
+          next_state = Ideal_lk;
+          done_r     = 1'b1;
+        end else begin
+          next_state = TCKL_lk;
+        end
       end else begin
-         DONE <= done_i;
-         if (enable_red == 1'b1) begin
-            tck_i       <= 1'b0;
-            tms_output  <= TMS_VECTOR;
-            tdi_output  <= TDI_VECTOR;
-            tdo_capture <= 0;
-         end else if (tck_en == 1'b1) begin
-            if (tck_pulse == 1'b1) begin
-               tck_i <= ~tck_i;
-               if (state == TCKH) begin
-                  tms_output  <= {1'b0, tms_output[31:1]};
-                  tdi_output  <= {1'b0, tdi_output[31:1]};
-                  tdo_capture <= {tdo_capture[30:0], TDO};
-               end else begin
-                  tdo_buffer[index] <= TDO;
-               end
-            end
-         end else begin
-            tms_output <= 0;
-            tdi_output <= 0;
-         end
+        next_state = TCKH_lk;
       end
+    end
+    
+    default : begin
+      next_state = Ideal_lk;
+    end
+  endcase
+end
+
+always @(posedge clk_i) begin
+   if (reset_i == 1'b1) begin
+    tck_cnt <= 0;
+    bit_cnt <= 0;
+    index     <= 0;
+   end else begin
+    if (en_edge == 1'b1) begin
+    tck_cnt <= 0;
+    bit_cnt <= length_i-1;
+    index     <= 0;
+   end else if (tck_en == 1'b1) begin
+    if (tck_cnt == (C_TCK_CLOCK_RATIO/2)-1) begin
+     tck_cnt <= 0;
+    end else begin
+     tck_cnt <= tck_cnt + 1;
+    end
+    //
+    if (state == TCKH_lk && tck_pulse) begin
+     bit_cnt <= bit_cnt - 1;
+     index     <= index + 1;
+    end
    end
-   
-   
+   end
+end
+
+assign tck_pulse = (tck_cnt == (C_TCK_CLOCK_RATIO/2)-1) ? 1'b1 : 1'b0;
+
+always @(posedge clk_i) begin
+   if (reset_i == 1'b1) begin
+   tck_r       <= 1'b0;
+   tms_r  <= 0;
+   tdi_r  <= 0;
+   tdo_capture <= 0;
+   done_o        <= 1'b0;
+   end else begin
+   done_o <= done_r;
+   if (en_edge == 1'b1) begin
+   tck_r       <= 1'b0;
+   tms_r  <= tms_vec_i;
+   tdi_r  <= tdi_vec_i;
+   tdo_capture <= 0;
+   end else if (tck_en == 1'b1) begin
+   if (tck_pulse == 1'b1) begin
+    tck_r <= ~tck_r;
+    if (state == TCKH_lk) begin
+    tms_r  <= {1'b0, tms_r[31:1]};
+    tdi_r  <= {1'b0, tdi_r[31:1]};
+    tdo_capture <= {tdo_capture[30:0], tdo_i};
+    end else begin
+    tdo_buffer[index] <= tdo_i;
+    end
+   end
+   end else begin
+   tms_r <= 0;
+   tdi_r <= 0;
+   end
+   end
+end
+
 genvar i;
 
 generate 
-    for (i=0; i < 32; i = i + 1) begin : array
-        assign tdo_capture2[i] = tdo_buffer[i];
-    end
+for (i=0; i < 32; i = i + 1) begin : array
+assign tdo_capture2[i] = tdo_buffer[i];
+end
 endgenerate
 
- //  assign TMS = tms_output[0] ? 1'bZ : 1'b0;
-assign TMS = tms_output[0];
+//  assign tms_o = tms_r[0] ? 1'bZ : 1'b0;
+assign tms_o = tms_r[0];
 //      OBUFT u0_OBUFT (
-//          .O (TMS) ,
+//          .O (tms_o) ,
 //          .I (1'b0) ,
-//          .T (~tms_output[0])
+//          .T (~tms_r[0])
 //     );
-     
-   //   assign TDI = tdi_output[0] ? 1'bZ : 1'b0;
-assign TDI = tdi_output[0];
+
+//   assign tdi_o = tdi_r[0] ? 1'bZ : 1'b0;
+assign tdi_o = tdi_r[0];
 //      OBUFT u1_OBUFT (
-//           .O (TDI) ,
+//           .O (tdi_o) ,
 //           .I (1'b0) ,
-//           .T (~tdi_output[0])
+//           .T (~tdi_r[0])
 //      );
-     
-   //   assign TCK = tck_i ? 1'bZ : 1'b0;
-assign TCK = tck_i;
+
+//   assign tck_o = tck_r ? 1'bZ : 1'b0;
+assign tck_o = tck_r;
 //      OBUFT u_OBUFT (
-//           .O (TCK) ,
+//           .O (tck_o) ,
 //           .I (1'b0) ,
-//           .T (~tck_i)
+//           .T (~tck_r)
 //      );
-assign TDO_VECTOR = tdo_capture2;
-   
+assign tod_vec_o = tdo_capture2;
+
 
 endmodule
